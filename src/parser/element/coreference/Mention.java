aa81c786916a590d13da89ec7f85bb606c2f7afc
David Alfter <alfter.david@gmx.net>
2014-11-26 11:25:46 +0100

    Added hashCode and equals methods.

--- src/parser/element/coreference/Mention.java
+++ src/parser/element/coreference/Mention.java
@@ -4,7 +4,7 @@ package parser.element.coreference;
  * @author David
  *
  */
-public class Mention {
+public class Mention implements Cloneable {
 	/**
 	 * Textual representation
 	 */
@@ -15,11 +15,54 @@ public class Mention {
 	 */
 	private int occursInSentenceNum;
 	
+	public Mention() {
+	
+	}
+	
+	public Mention (String textMention, int occursIn) {
+		this.textMention = textMention;
+		this.occursInSentenceNum = occursIn;
+	}
+	
 	public String getTextMention () {
 		return textMention;
 	}
 	
 	public int getOccursInSentenceNum () {
 		return occursInSentenceNum;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + occursInSentenceNum;
+		result = prime * result
+				+ ((textMention == null) ? 0 : textMention.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mention other = (Mention) obj;
+		if (occursInSentenceNum != other.occursInSentenceNum)
+			return false;
+		if (textMention == null) {
+			if (other.textMention != null)
+				return false;
+		} else if (!textMention.equals(other.textMention))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public Mention clone () {
+		return new Mention(this.textMention, this.occursInSentenceNum);
 	}
 }
