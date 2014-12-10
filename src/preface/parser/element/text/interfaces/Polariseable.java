191d6556c153653393f60e4139bb56756905f2db
David Alfter <alfter.david@gmx.net>
2014-12-10 14:25:16 +0100

    Added polariseable interface.

---
+++ src/preface/parser/element/text/interfaces/Polariseable.java
@@ -0,0 +1,10 @@
+package preface.parser.element.text.interfaces;
+
+public interface Polariseable {
+
+	public String getString();
+	public void setPolarity(double d);
+	public double getPolarity();
+	public boolean isPolarized();
+	
+}
