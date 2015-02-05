81f661e7afbb6e75461ac9d329b37d1644bfb719
daalft <blueygh2@gmail.com>
2015-02-05 09:30:47 +0100

    Added Text parsing.

--- src/preface/parser/Parser.java
+++ src/preface/parser/Parser.java
@@ -11,6 +11,10 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
+import preface.parser.element.NEType;
+import preface.parser.element.text.AnnotatedWord;
+import preface.parser.element.text.Chapter;
+import preface.parser.element.text.Sentence;
 import preface.parser.element.text.Text;
 import preface.parser.element.coreference.*;
 /**
@@ -31,19 +35,19 @@ public class Parser {
 		chains = new ArrayList<Chain>();
 		text = new Text();
 	}
-	
+
 	public List<Entity> getEntities () {
 		return entities;
 	}
-	
+
 	public List<Chain> getChains (){
 		return chains;
 	}
-	
+
 	public Text getText () {
 		return text;
 	}
-	
+
 	public void dispose () {
 		entities = null;
 		text = null;
@@ -58,85 +62,141 @@ public class Parser {
 	public void parse(File dir) {
 		File[] fileList = dir.listFiles();
 		int chptNr = 0;
+		Chapter chapter = null;
+		Sentence sentence = null;
+		AnnotatedWord annotatedWord = null;
+
 		for (File f : fileList){
-		try {
-			Entity currEnt = null;		//enth�lt die entity (coreference) mentions werden per .add(mention) zu der entity hinzugef�gt.
-			Mention currMen = null;
-			InputStream	in = new FileInputStream(f);
-			XMLInputFactory factory = XMLInputFactory.newInstance();
-			XMLStreamReader parser = factory.createXMLStreamReader(in);
-			
-			while (parser.hasNext())
-			{
-				switch (parser.getEventType() )
+			try {
+				Entity currEnt = null;		//enth�lt die entity (coreference) mentions werden per .add(mention) zu der entity hinzugef�gt.
+				Mention currMen = null;
+				InputStream	in = new FileInputStream(f);
+				XMLInputFactory factory = XMLInputFactory.newInstance();
+				XMLStreamReader parser = factory.createXMLStreamReader(in);
+
+				while (parser.hasNext())
 				{
+					switch (parser.getEventType() )
+					{
 					case XMLStreamConstants.END_DOCUMENT:
 						parser.close();
-					break;
-						
+						break;
+
 					case XMLStreamConstants.START_ELEMENT:
 						switch(parser.getLocalName())
 						{
-							case "chapter":
-								chptNr= Integer.parseInt(parser.getAttributeValue(0)); // Takes the chapter ID from the XML file
-								
-							case "coreference":
-								currEnt = new Entity();
-								currEnt.setId(Integer.parseInt(parser.getAttributeValue(0))); // �bernimmt die ID's aus <coreference id="xx">
-								currEnt.setChapterNumber(chptNr);
+						case "chapter":
+							chptNr= Integer.parseInt(parser.getAttributeValue(0)); // Takes the chapter ID from the XML file
+							chapter = new Chapter();
+							chapter.setChapterNumber(chptNr);
 							break;
-								
-							case "mention":
-								currMen = new Mention();
-								if(parser.getAttributeValue(0) != null){currMen.setRepresentative(true);} //set if <mention representative = "true">
+						case "coreference":
+							currEnt = new Entity();
+							currEnt.setId(Integer.parseInt(parser.getAttributeValue(0))); // �bernimmt die ID's aus <coreference id="xx">
+							currEnt.setChapterNumber(chptNr);
+							break;
+
+						case "mention":
+							currMen = new Mention();
+							if(parser.getAttributeValue(0) != null){currMen.setRepresentative(true);} //set if <mention representative = "true">
 							break; 
-							
-							case "start":
-								currMen.setWordNumberStart(Integer.parseInt(parser.getElementText()));
+
+						case "start":
+							currMen.setWordNumberStart(Integer.parseInt(parser.getElementText()));
 							break;
-								
-							case "end":
-								currMen.setWordNumberEnd(Integer.parseInt(parser.getElementText()));
+
+						case "end":
+							currMen.setWordNumberEnd(Integer.parseInt(parser.getElementText()));
 							break;
-							
-							case "head":
-								currMen.setWordNumberHead(Integer.parseInt(parser.getElementText()));
+
+						case "head":
+							currMen.setWordNumberHead(Integer.parseInt(parser.getElementText()));
 							break;
-							
-							case "text":
-								currMen.setTextMention(parser.getElementText());
+
+						case "text":
+							currMen.setTextMention(parser.getElementText());
 							break;
-							
-							default:
+						case "sentence":
+							String att = parser.getAttributeValue(0);
+							if (att == null) {
+								currMen.setOccursInSentenceNum(Integer.parseInt(parser.getElementText()));
+							} else {
+								sentence = new Sentence();
+								sentence.setSentenceNumber(Integer.parseInt(parser.getAttributeValue(0)));
+							}
+							break;
+						case "token":
+							annotatedWord = new AnnotatedWord();
+							annotatedWord.setId(Integer.parseInt(parser.getAttributeValue(0)));
+							break;
+						case "word":
+							annotatedWord.setWord(parser.getElementText());
+							break;
+						case "lemma":
+							annotatedWord.setLemma(parser.getElementText());
+							break;
+						case "POS":
+							annotatedWord.setPOS(parser.getElementText());
+							break;
+						case "NER":
+							switch(parser.getElementText()) {
+							case "O":
+								annotatedWord.setType(NEType.OTHER);
+								break;
+							case "PERSON":
+								annotatedWord.setType(NEType.PERSON);
+								break;
+							case "LOCATION":
+								annotatedWord.setType(NEType.LOCATION);
+								break;
+							case "NUMBER":
+								annotatedWord.setType(NEType.NUMBER);
+								break;
+								// if we encounter some other NE Type not defined
+								// set NEType to OTHER
+								default:
+									annotatedWord.setType(NEType.OTHER);
+									break;
+							} 
+						
+						default:
 							break;
 						}
-					break;
+						break;
 
 					case XMLStreamConstants.END_ELEMENT:
 						switch (parser.getLocalName())
 						{
-							case "coreference":
-								entities.add(currEnt); //f�gt currEnt der Liste der entities hinzu
+						case "coreference":
+							entities.add(currEnt); //f�gt currEnt der Liste der entities hinzu
 							break;
-							
-							case "mention":
-								currEnt.add(currMen);
+
+						case "mention":
+							currEnt.add(currMen);
 							break;	
-								
-							default:
+						case "chapter":
+							text.add(chapter);
+							break;
+						case "sentence":
+							chapter.add(sentence);
+							break;
+						case "token":
+							sentence.add(annotatedWord);
+							break;
+						default:
 							break;
 						}
-					break;
+						break;
+					}
+					parser.next();
 				}
-				parser.next();
+			} catch (FileNotFoundException e){
+				e.printStackTrace();
+			} catch (XMLStreamException e){
+				e.printStackTrace();
 			}
-		} catch (FileNotFoundException e){
-			e.printStackTrace();
-		} catch (XMLStreamException e){
-			e.printStackTrace();
+
 		}
-		
-	}
 	}
 	public void parseIndex(File indexFile){
 		try{
@@ -145,53 +205,53 @@ public class Parser {
 			InputStream	in = new FileInputStream(indexFile);
 			XMLInputFactory factory = XMLInputFactory.newInstance();
 			XMLStreamReader parser = factory.createXMLStreamReader(in);
-			
+
 			while(parser.hasNext()){
 				switch(parser.getEventType())
 				{
-					case XMLStreamConstants.END_DOCUMENT:
-						parser.close();
+				case XMLStreamConstants.END_DOCUMENT:
+					parser.close();
 					break;
-				
-					case XMLStreamConstants.START_ELEMENT:
-						switch(parser.getLocalName()){
-							case "chain":
-								currChn = new Chain();
-								currChn.setText(parser.getAttributeValue(0));
-							break;
-							case "coreference":
-								currRef = new EntityReference();					
-							break;
-							case "id":
-								currRef.setId(Integer.parseInt(parser.getElementText()));
-							break;
-							case "chapter":
-								currRef.setChapterNumber(Integer.parseInt(parser.getElementText()));
-							break;
-							default:
-							break;
-						}
-					case XMLStreamConstants.END_ELEMENT:
-						switch(parser.getLocalName()){
-							case "chain":
-								chains.add(currChn);
-							break;
-							
-							case "coreference":
-								currChn.add(currRef);
-							break;
-							default:
-							break;
-						}
-				break;	
+
+				case XMLStreamConstants.START_ELEMENT:
+					switch(parser.getLocalName()){
+					case "chain":
+						currChn = new Chain();
+						currChn.setText(parser.getAttributeValue(0));
+						break;
+					case "coreference":
+						currRef = new EntityReference();					
+						break;
+					case "id":
+						currRef.setId(Integer.parseInt(parser.getElementText()));
+						break;
+					case "chapter":
+						currRef.setChapterNumber(Integer.parseInt(parser.getElementText()));
+						break;
+					default:
+						break;
+					}
+				case XMLStreamConstants.END_ELEMENT:
+					switch(parser.getLocalName()){
+					case "chain":
+						chains.add(currChn);
+						break;
+
+					case "coreference":
+						currChn.add(currRef);
+						break;
+					default:
+						break;
+					}
+					break;	
 				}
-			parser.next();
+				parser.next();
 			}
-			
-			
-			
-			
-			
+
+
+
+
+
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
@@ -199,22 +259,22 @@ public class Parser {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String args[]) {
-		/**
-		 * Development only. Not used in the later process.
-		 *
-		File dir = new File("data\\UncleTomsCabin\\chapters\\extracted");
-		Parser p = new Parser();
-		p.parse(dir);
-		List<Entity> entities = p.getEntities();
-		System.out.println(entities);
-		**/
-		
-		Parser p = new Parser();
-		p.parseIndex(new File("data\\coref_index.xml"));
-		List<Chain> chains = p.getChains();
-		System.out.println(chains);
-		
-	}
+
+//	public static void main(String args[]) {
+//		/**
+//		 * Development only. Not used in the later process.
+//		 *
+//		File dir = new File("data\\UncleTomsCabin\\chapters\\extracted");
+//		Parser p = new Parser();
+//		p.parse(dir);
+//		List<Entity> entities = p.getEntities();
+//		System.out.println(entities);
+//		 **/
+//
+//		Parser p = new Parser();
+//		p.parseIndex(new File("data\\coref_index.xml"));
+//		List<Chain> chains = p.getChains();
+//		System.out.println(chains);
+//
+//	}
 }
