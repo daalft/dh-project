56005da0f7cf91d00a2996611f4a8755f4c133da
David Alfter <alfter.david@gmx.net>
2015-02-05 14:27:16 +0100

    Moved links from Result to ManaCore.

--- src/preface/analysis/result/Result.java
+++ src/preface/analysis/result/Result.java
@@ -8,27 +8,12 @@ import preface.parser.element.text.AnnotatedWord;
 public class Result {
 
 	/**
-	 * Links between named entities
-	 * EntityID => raw Frequency
-	 */
-	private HashMap<Integer, Integer> links;
-	
-	/**
 	 * Chapter -> (word->frequency)
 	 */
 	private HashMap<Integer, FrequencyTable> words;
 	
 	public Result() {
-		links = new HashMap<>();
 		words = new HashMap<>();
-	}
-	
-	public void link(int i) {
-		if (links.containsKey(i)) {
-			links.put(i, links.get(i)+1);
-		} else {
-			links.put(i, 1);
-		}
 	}
 
 	public void add(AnnotatedWord w, int chapter) {
@@ -42,15 +27,11 @@ public class Result {
 	}
 
 	public boolean isEmpty() {
-		return (links.isEmpty() && words.isEmpty());
+		return words.isEmpty();
 	}
 	
 	public String toString () {
 		StringBuilder sb = new StringBuilder("[Result]:\n");
-		sb.append("Links:\n");
-		for (Entry<Integer, Integer> link : links.entrySet()) {
-			sb.append(link.getKey().intValue()).append("\t").append(link.getValue().intValue()).append("\n");
-		}
 		sb.append("\n\nChapters:\n");
 		for (Entry<Integer, FrequencyTable> e : words.entrySet()) {
 			sb.append("Chapter ").append(e.getKey().intValue()).append("\n");
@@ -58,10 +39,6 @@ public class Result {
 		}
 		sb.append("\n");
 		return sb.toString();
-	}
-	
-	public HashMap<Integer, Integer> getLinks () {
-		return links;
 	}
 
 	public String toJSON() {
